64f846198e5e3c741103400a1d51d77c6d5de9a6
miguel <miguelpositive@gmail.com>
2023-11-23 19:14:17 -0500

    cambio

--- src/components/context/Context.jsx
+++ src/components/context/Context.jsx
@@ -129,7 +129,7 @@ const Context = ({ children }) => {
 
   const createAlert = async () => {
     try {
-      await axios.post("https://note-cobro.onrender.com/create-alert", {
+      await axios.post("http://localhost:4000/create-alert", {
         _id: debtorTemp._id,
         debtorTemp,
         payDay,
